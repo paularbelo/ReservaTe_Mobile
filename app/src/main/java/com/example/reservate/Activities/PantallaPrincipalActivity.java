a7940ade91bea0ba12e19b662795e7d93cbbfd24
Matias Melfi <matiasnicolasmelfi@gmail.com>
2020-02-14 00:23:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/reservate/Activities/PantallaPrincipalActivity.java
@@ -0,0 +1,17 @@
+package com.example.reservate.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.reservate.R;
+
+public class PantallaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pincipal);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/reservate/Activities/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.reservate.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.reservate.MainActivity;
+import com.example.reservate.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, PantallaPrincipalActivity.class);
+                startActivity(intent);
+
+            }
+        }, 4000);
+    }
+}
