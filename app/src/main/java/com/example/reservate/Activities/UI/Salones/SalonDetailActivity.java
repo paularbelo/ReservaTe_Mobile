d00391eabdc24904c014ea56a2271dc3660b0ffc
Matias Melfi <matiasnicolasmelfi@gmail.com>
2020-02-16 21:52:29 -0300

    detail sucursales

---
+++ app/src/main/java/com/example/reservate/Activities/UI/Salones/SalonDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.reservate.Activities.UI.Salones;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.reservate.R;
+
+public class SalonDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salon_detail);
+    }
+}
