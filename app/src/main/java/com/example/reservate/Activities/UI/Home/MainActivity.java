3887d6837cb4ae701323a5bc588beb6f96794455
Matias Melfi <matiasnicolasmelfi@gmail.com>
2020-02-23 12:22:47 -0300

    23/2/2020 - Cambios Layout BottomNavigation

--- app/src/main/java/com/example/reservate/Activities/UI/Home/MainActivity.java
+++ app/src/main/java/com/example/reservate/Activities/UI/Home/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.reservate.Activities.UI.Home;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -15,67 +12,42 @@ import com.example.reservate.Activities.UI.Fragments.FramentHelp;
 import com.example.reservate.Activities.UI.Fragments.SalonesFragment;
 import com.example.reservate.R;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawer;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.salones:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SalonesFragment()).commit();
+                    break;
+                case R.id.profile:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentProfile()).commit();
+                    break;
+                case R.id.reservas:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentReservas()).commit();
+                    break;
+                case R.id.help:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FramentHelp()).commit();
+                    break;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.support.v7.widget.Toolbar toolbar= findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        drawer=findViewById(R.id.drawer_layout);
-        NavigationView navView=findViewById(R.id.nav_view);
-        navView.setNavigationItemSelectedListener(this);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        ActionBarDrawerToggle toogle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toogle);
-        toogle.syncState();
 
-        if(savedInstanceState==null){
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SalonesFragment()).commit();
-            navView.setCheckedItem(R.id.nav_home);
-        }
-
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-        switch (menuItem.getItemId()){
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SalonesFragment()).commit();
-                break;
-        }
-        switch (menuItem.getItemId()){
-            case R.id.nav_profile:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentProfile()).commit();
-                break;
-        }
-        switch (menuItem.getItemId()){
-            case R.id.nav_reservas:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentReservas()).commit();
-                break;
-        }
-        switch (menuItem.getItemId()){
-            case R.id.nav_help:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FramentHelp()).commit();
-                break;
-        }
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 }
